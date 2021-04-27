222fde2b25f488723c6a877b6cdda12552769206
Emmanuel Genard <edgenard@gmail.com>
2021-04-26 20:35:07 -0400

    Add todo item has many statuses and priorities

--- week-3/todo.ts
+++ week-3/todo.ts
@@ -1,19 +1,37 @@
+enum ItemState {
+  DONE = 'Done',
+  NOT_DONE = 'Not Done',
+  IN_PROGRESS = 'In Progress',
+  UNDER_REVIEW = 'Under Review',
+  BLOCKED = 'Blocked'
+}
 
+enum ItemPriority {
+  HIGH = 1,
+  MEDIUM = 2,
+  LOW = 3
+}
 
 interface TodoItem {
   title: string,
-  done: true | false
+  state: ItemState
+  priority: ItemPriority
 }
 
 interface TodoList {
   items: Array<TodoItem>,
 }
 
+interface TodoListFilter {
+  filterByStatus(list: TodoList, status: ItemState): TodoList,
+  filterByPriority(list: TodoList, priority: ItemPriority): TodoList
+}
 
 interface TodoApp {
-  list: TodoList
+  list: TodoList,
+  filter: TodoListFilter,
   createItem(itemTitle: string): TodoList,
   deleteItem(item: TodoItem): TodoList,
-  viewList(): TodoList
-  markDone(item: TodoItem): TodoList
+  viewList(): TodoList,
+  updateStatus(item: TodoItem, status: ItemState): TodoList,
 }
