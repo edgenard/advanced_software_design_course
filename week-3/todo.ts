924c5c3f259fbb84e6fb0b1e383c449b7ecffeff
Emmanuel Genard <edgenard@gmail.com>
2021-04-26 20:15:18 -0400

    First round of design dones

--- week-3/todo.ts
+++ week-3/todo.ts
@@ -1,14 +1,19 @@
 
 
-type TaskDone = true | false
+interface TodoItem {
+  title: string,
+  done: true | false
+}
 
-class Task {
-  done: TaskDone
-
-  constructor() {
-    this.done = false;
-
-  }
+interface TodoList {
+  items: Array<TodoItem>,
+}
 
 
+interface TodoApp {
+  list: TodoList
+  createItem(itemTitle: string): TodoList,
+  deleteItem(item: TodoItem): TodoList,
+  viewList(): TodoList
+  markDone(item: TodoItem): TodoList
 }
